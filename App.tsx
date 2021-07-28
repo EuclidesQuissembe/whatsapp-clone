783537883e456cd709d638e300d8c2211002e9b4
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-07-28 19:10:50 +0100

    :tada: Initial Commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <View />;
+};
+
+export default App;
